b8231e7b2d4cc27d51e7427e1bd7c4d40cc87d16
Hitaishi08 <125145810+Hitaishi08@users.noreply.github.com>
2024-06-20 09:28:38 +0000

    loops

---
+++ basic/loops05.js
@@ -0,0 +1,45 @@
+// types of loops
+/* 
+    1. for loop
+    2. for in loop
+    3. for of loop
+    4. while loop
+    5. do while loop
+*/
+
+let prompt = require("prompt-sync")();
+
+// FOR LOOP
+
+/*
+let sum = 0;
+let  n = prompt("enter the number: ");
+for(let i =0;i<=n;i++)
+    {
+        sum += i;
+    }
+console.log("sum: ",sum);
+*/
+
+// FOR IN LOOP (also work with arrays)
+// loop through the keys of the object
+let obj = 
+{
+    a: 95,
+    b: 45,
+    c: 56,
+    d: 57,
+    e: 23
+}
+
+for(let i in obj)
+    {
+        console.log("marks of " + i + " are " + obj[i] )
+    }
+
+// FOR OF LOOP (it does not work on object)
+// it loops through the values 
+for(let b of "hitaishi")
+    {
+        console.log(b);
+    }
